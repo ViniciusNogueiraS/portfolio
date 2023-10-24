6cbb1cabdd73e05e5f9d71bb03ba206b2940e1c4
Vinícius Nogueira <vinicius.nogueira@agenciam3.com>
2023-10-23 22:57:30 -0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 import Perfil from './components/Perfil';
+
+import imagePerfil from './assets/images/perfil0623.jpeg';
 
 function App() {
   return (
@@ -10,7 +15,7 @@ function App() {
       <header>
         <Perfil/>
         <div className="Col Imagem">
-          <img src={''}/>
+          <img src={imagePerfil}/>
         </div>
       </header>
       <div className="Body"
@@ -18,19 +23,20 @@ function App() {
            fontSize: '20px'
         }}
       >
-        <article>
-          <h3>Experiências</h3>
-
+        <h3>Experiências</h3>
+        <article className="Experiencias">
+          <Skeleton count={1} height={'250px'}/>
+          <Skeleton count={1} height={'250px'}/>
+          <Skeleton count={1} height={'250px'}/>
         </article>
-        <article>
-          <h3>Projetos</h3>
-
+        <h3>Projetos</h3>
+        <article className="Projetos">
+          <AliceCarousel mouseTracking items={[]}/>
         </article>
-        <article>
-          <h3>Skills</h3>
-
+        <h3>Skills</h3>
+        <article className="Skills">
+          <Skeleton count={6} height={'14px'}/>
         </article>
-        <Skeleton count={1} height={'100px'}/>
       </div>
       <footer>
         <div className="Col">
